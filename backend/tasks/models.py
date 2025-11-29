d1a4724f64ce75d7ae68ede26bc03e9956f15767
csmax729 <vcs171427@gmail.com>
2025-11-29 10:29:35 +0530

    Add Task model with fields for title and due date

---
+++ backend/tasks/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Task(models.Model):
+    title = models.CharField(max_length=255)
+    due_date = models.DateField()
+    estimated_hours = models.FloatField()
+    importance = models.IntegerField()  # 1-10 scale
+    dependencies = models.JSONField(default=list)
+
+    def __str__(self):
+        return self.title
